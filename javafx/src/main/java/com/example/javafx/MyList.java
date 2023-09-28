765cb429cf721684389b3517d8bf918d9d8bf540
OsamaAbuKamel <92945728+OsamaAbuKamel@users.noreply.github.com>
2023-09-28 09:57:45 +0300

    Create MyList.java

---
+++ javafx/src/main/java/com/example/javafx/MyList.java
@@ -0,0 +1,60 @@
+package com.example.javafx;
+
+import com.example.javafx.ListInterface;
+
+import java.util.Arrays;
+
+public class MyList<T extends Comparable<T>> implements ListInterface<T> {
+    
+    T[] list;
+    int count;
+    
+    public MyList(int size) {
+        list = (T[]) new Comparable[size];
+        count = 0;
+    }
+    
+    @Override
+    public void add(T data) {
+        if (count < list.length) {
+            list[count] = data;
+            count++;
+        } else
+            System.out.println("List is full");
+    }
+    
+    @Override
+    public void remove(T data) {
+        for (int i = 0; i < count; i++) {
+            if (list[i].equals(data)) {
+                list[i] = list[count - 1];
+                count--;
+            } else
+                System.out.println("Element not found");
+        }
+    }
+    
+    @Override
+    public int find(T data) {
+        for (int i = 0; i < count; i++) {
+            if (data.compareTo(list[i]) == 0)
+                return i;
+        }
+        return -1;
+    }
+    
+    @Override
+    public T getIndex(int index) {
+        return list[index];
+    }
+    public int size() {
+        return count;
+    }
+    @Override
+    public void printList() {
+        //print list
+        for (int i = 0; i < count; i++) {
+            System.out.println(list[i]);
+        }
+    }
+}

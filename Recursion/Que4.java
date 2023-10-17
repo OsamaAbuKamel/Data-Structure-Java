8e4bd3b11783b7ac3595fe202617f69ba5d0dd79
OsamaAbuKamel <92945728+OsamaAbuKamel@users.noreply.github.com>
2023-10-17 15:53:45 +0300

    Create Que4.java

---
+++ Recursion/Que4.java
@@ -0,0 +1,24 @@
+public class Que4 {
+    public static void main(String[] args) {
+        String n = "cat is running"; ;
+        System.out.println(reverseWords(n));
+    }
+    public static String  reverseWords(String s) {
+        //Check if the string is null or empty or does not contain any spaces
+        if (s == null || s.isEmpty() ||!s.trim().contains(" "))
+            return s;
+        //Trim the string to remove any leading or trailing spaces
+        s=s.trim();
+        //Find the last space in the string
+        int lastSpace = s.lastIndexOf(' ');
+        //Get the last word from the string
+        String lastWord = s.substring(lastSpace+1);
+        //Return the last word reversed plus the reversed remaining part of the string
+        return lastWord +" "+  reverseWords(s.substring(0, lastSpace));
+//            String word[] = s.split(" ");
+//            String lastWord = word[word.length - 1];
+//            return lastWord + " " + reverseWords(s.substring(0, s.length() - lastWord.length()));
+//
+        }
+    
+}

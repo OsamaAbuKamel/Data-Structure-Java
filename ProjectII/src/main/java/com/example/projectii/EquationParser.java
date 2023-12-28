8fa7d14bda05fa95a948df85b21a57f7407a6162
OsamaAbuKamel <92945728+OsamaAbuKamel@users.noreply.github.com>
2023-12-28 11:11:28 +0200

    ProjectII

--- ProjectII/src/main/java/com/example/projectii/EquationParser.java
+++ ProjectII/src/main/java/com/example/projectii/EquationParser.java
@@ -2,41 +2,130 @@ package com.example.projectii;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class EquationParser {
 
-    public boolean isBalanced(String input, CStack<String> stack) {
+    public boolean isBalanced(String exp,CStack<String> stack){
+       
         int top = stack.createStack();
-        String[] words = input.split("");
-        for (String s : words) {
-            if (isOpenTag(s)) {
-                stack.push(s, top);
-            }
-            if (isCloseTag(s)) {
-                if (!isPair(stack.peek(top), s)) {
+        char[] chars = exp.toCharArray();
+        for (char c : chars ) {
+            if (c=='<') {
+                stack.push(String.valueOf(c),top);
+            }else if (c=='>') {
+                if (stack.isEmpty(top)) {
                     return false;
                 }
-                stack.pop(top);
+                char open = stack.pop(top).charAt(0);
+                if (!(open=='<'&& c=='>')) {
+                    return false;
+                }
             }
         }
-
         return stack.isEmpty(top);
     }
 
-    private boolean isOpenTag(String tag) {
-        return tag.equals("<242>") || tag.equals("<section>") || tag.equals("<infix>")
-                || tag.equals("<postfix>") || tag.equals("<equation>");
+    
+    public String extractInfixPostFix(String exp,int i) {
+        String sections = extractSections(exp, i);
+        StringBuilder result = new StringBuilder();
+        for (String section : sections.split("\n\n")) {
+            ArrayList<String> equations = extractInfixPostfixFromSection(section);
+            result.append("Infix Equations: ").append(equations.get(0)).append("\n");
+            result.append("Postfix Equations: ").append(equations.get(1)).append("\n\n");
+        }
+        return result.toString();
     }
 
-    private boolean isCloseTag(String tag) {
-        return tag.equals("</242>") || tag.equals("</section>") || tag.equals("</infix>")
-                || tag.equals("</postfix>") || tag.equals("</equation>");
+    private ArrayList<String> extractInfixPostfixFromSection(String section) {
+        ArrayList<String> result = new ArrayList<>();
+        ArrayList<String> infixEquations = extractEquations(section, "<infix>", "</infix>");
+        ArrayList<String> postfixEquations = extractEquations(section, "<postfix>", "</postfix>");
+        result.add(infixEquations.get(0)); // Add the first (and possibly only) infix equation
+        result.add(postfixEquations.get(0)); // Add the first (and possibly only) postfix equation
+        return result;
     }
 
-    private boolean isPair(String openTag, String closeTag) {
-        return openTag != null && openTag.equals(closeTag);
+    private ArrayList<String> extractEquations(String section, String startTag, String endTag) {
+        ArrayList<String> equations = new ArrayList<>();
+        String escapedStartTag = startTag.replaceAll("([.*+?^=!:${}()|[\\]/\\\\])", "\\\\$1");
+        String escapedEndTag = endTag.replaceAll("([.*+?^=!:${}()|[\\]/\\\\])", "\\\\$1");
+        
+        // Use the escaped tags in the regular expression for splitting
+        String[] splitExp = section.split(escapedStartTag + "|" + escapedEndTag);
+        for (String string : splitExp) {
+            // String equation
+        }
+                // int startIndex = section.indexOf(startTag);
+        // int endIndex = section.indexOf(endTag);
+        // while (startIndex != -1 && endIndex != -1) {
+        //     String equation = section.substring(startIndex + startTag.length(), endIndex);
+        //     equation = equation.replace("<equation>", "").replace("</equation>", ","); // Remove the equation tags
+        //     equations.add(equation);
+        //     startIndex = section.indexOf(startTag, endIndex + endTag.length());
+        //     endIndex = section.indexOf(endTag, startIndex + startTag.length());
+        // }
+        return equations;
     }
+    
+    private String extractSections(String exp,int i) {
+        ArrayList<String> sections = new ArrayList<>();
+        String[] splitExp = exp.split("<section>|</section>");
+        for (int index = 1; index < splitExp.length; index++) {
+            sections.add(splitExp[index]);
+        }
+        return sections.get(i);
+    }
+    // public String extractSections(String exp,int i){
+    //     ArrayList<String> sections = new ArrayList<>();
+    //     String startTag = "<section>";
+    //     String endTag = "</section>";
+    //     int startIndex = String.valueOf(exp).indexOf(startTag);
+    //     int endIndex = String.valueOf(exp).indexOf(endTag);
+    //     while (startIndex != -1 && endIndex != -1) {
+    //         sections.add(exp.substring(startIndex + startTag.length(), endIndex));
+    //         startIndex = String.valueOf(exp).indexOf(startTag, endIndex + endTag.length());
+    //         endIndex = String.valueOf(exp).indexOf(endTag, startIndex + startTag.length());
+    //     }
+        
+    //     return sections.get(i);
+        // String startTag = "<section>";
+        // String endTag = "</section>";
+        // int startIndex = String.valueOf(exp).indexOf(startTag);
+        // int endIndex = String.valueOf(exp).indexOf(endTag);
+        //     if (startIndex != -1 && endIndex != -1) {
+        //     // return exp.substring(startIndex + startTag.length(), endIndex);
+        // }
+        // String startTag1 = "<infix>";
+        // String endTag1 = "</infix>";
+        // startIndex = String.valueOf(exp).indexOf(startTag1);
+        // endIndex = String.valueOf(exp).indexOf(endTag1);
+        //     if (startIndex != -1 && endIndex != -1) {
+        //     // return exp.substring(startIndex + startTag1.length(), endIndex);
+        // }
+        // String startTag2 = "<postfix>";
+        // String endTag2 = "</postfix>";
+        // startIndex = String.valueOf(exp).indexOf(startTag2);
+        // endIndex = String.valueOf(exp).indexOf(endTag2);
+        //     if (startIndex != -1 && endIndex != -1) {
+        //     // return exp.substring(startIndex + startTag2.length(), endIndex);
+        // }
+        // String startTag3 = "<equation>";
+        // String endTag3 = "</equation>";
+        // startIndex = String.valueOf(exp).indexOf(startTag3);
+        // endIndex = String.valueOf(exp).indexOf(endTag3);
+        //     if (startIndex != -1 && endIndex != -1) {
+        //     // return exp.substring(startIndex + startTag3.length(), endIndex);
+        // }
+
+        
+        // return null;
+    // }
 
     public String readFile(String filePath) {
         StringBuilder result = new StringBuilder();
@@ -79,4 +168,5 @@ public class EquationParser {
         }
         return sb.toString();
     }
+
 }
--- ProjectII/src/main/java/com/example/projectii/Main.java
+++ ProjectII/src/main/java/com/example/projectii/Main.java
@@ -7,27 +7,32 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Converter c = new Converter();
-        String postfix = "2.57*";
-        String prefix = c.postfixToPrefix(postfix, stack);
-        System.out.println("Prefix expression: " + prefix);
-        // evaluate prefix
-        System.out.println("Result: " + c.evaluatePrefix(prefix, stack));
+        // String postfix = "2.57*";
+        // String prefix = c.postfixToPrefix(postfix, stack);
+        // System.out.println("Prefix expression: " + prefix);
+        // // evaluate prefix
+        // System.out.println("Result: " + c.evaluatePrefix(prefix, stack));
 
 
-        System.out.println("=== === === === === === === === === === === === === === === === === === === === ===");
+        // System.out.println("=== === === === === === === === === === === === === === === === === === === === ===");
 
-        String infixExpression = "5 + 3 * 4.5";
-        String postfixExpression = c.infixToPostfix(infixExpression, stack);
-        System.out.println("Infix expression: " + infixExpression);
-        System.out.println("Postfix expression: " + postfixExpression);
-        System.out.println(c.evaluatePostfix(postfixExpression, stack));
+        // String infixExpression = "( 15 + 3 ) * ( 4 ^ 2 )";
+        // String postfixExpression = c.infixToPostfix(infixExpression, stack);
+        // System.out.println("Infix expression: " + infixExpression);
+        // System.out.println("Postfix expression: " + postfixExpression);
+        // System.out.println(c.evaluatePostfix(postfixExpression, stack));
         EquationParser ep = new EquationParser();
         String file =("C:\\Users\\osama\\DataStructure\\Data-Structure-\\ProjectII\\src\\main\\resources\\com\\example\\projectii\\DS-Proj2 (1).242");
-        String s = ep.readFile(file);
-        String ss = ep.extractTag(s);
-        // System.out.println(s);
+                String s = ep.readFile(file);
+                // System.out.println(s);
+
+        System.out.println( ep.extractInfixPostFix(s,2));
+        // ep.extractEquations(file);
+        // String ss = ep.extractTag(s);
+        // // System.out.println(s);
         // System.out.println("=== === === === === === === === === === === === === === === === === === === === ===");
-        // System.out.println(ss);
+        // // System.out.println(ss);
+        // System.out.println(ep.extractEquation(s));
         // System.out.println("=== === === === === === === === === === === === === === === === === === === === ===");
         // System.out.println(ep.isBalanced(ss, stack));
     }

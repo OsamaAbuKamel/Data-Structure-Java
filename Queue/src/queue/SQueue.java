9c88424e4e5319c0dadc0cfdee85f89dc6124373
OsamaAbuKamel <92945728+OsamaAbuKamel@users.noreply.github.com>
2024-01-19 17:12:20 +0200

    Update SQueue.java

--- Queue/src/queue/SQueue.java
+++ Queue/src/queue/SQueue.java
@@ -41,4 +41,15 @@ public class SQueue<T extends Comparable<T>> implements Queueable<T> {
     public void clear() {
         stack.clear();
     }
+    public static void main(String[] args) {
+        SQueue<Integer> queue = new SQueue<>();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        while (!queue.isEmpty()) {
+            System.out.println(queue.dequeue());
+        }
+    }
 }
